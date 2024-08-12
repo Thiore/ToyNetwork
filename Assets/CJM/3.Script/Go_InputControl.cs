beb9d53812f2bafd043be9d824402bf6d03b2366
최정문 <>
2024-08-12 18:31:16 +0900

    오목 두기 전 두기 개발

---
+++ Assets/CJM/3.Script/Go_InputControl.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+enum PlayerColor
+{
+    Black = 0,
+    White
+}
+
+public class Go_InputControl : MonoBehaviour
+{
+    int myColor = 0;
+    [SerializeField] private Transform[] chipPivots;
+    [SerializeField] private GameObject chip_pivotParent;
+
+    // 0번 검은색 / 1번 흰색 
+    [SerializeField] private Material[] chip_material;
+    [SerializeField] private Material[] checkchip_material;
+
+    [SerializeField] Mesh mesh;
+
+    private void Awake()
+    {
+        mesh = Resources.Load("Gogame_chip") as Mesh;
+        chip_pivotParent = GameObject.Find("Chip_Pivot");
+        chipPivots = new Transform[chip_pivotParent.transform.childCount];
+        Debug.Log(chip_pivotParent.transform.childCount);
+
+    }
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        CheckPutChip(ray);
+        PutChip(ray);
+    }
+
+    private void PutChip(Ray ray)
+    {
+        //내가 0이거나 0이 아니거나로 두면 될듯 
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+
+                    Debug.Log(hit.collider.gameObject.name);
+                    hit.collider.gameObject.GetComponent<MeshFilter>().mesh = mesh;
+                    MeshRenderer mate = hit.collider.gameObject.GetComponent<MeshRenderer>();
+                    mate.material = myColor.Equals(0) ? chip_material[0] : chip_material[1];
+                }
+            }
+        }
+    }
+
+    private void CheckPutChip(Ray ray)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+                    hit.collider.gameObject.GetComponent<MeshFilter>().mesh = mesh;
+                    MeshRenderer mate = hit.collider.gameObject.GetComponent<MeshRenderer>();
+                    mate.material = myColor.Equals(0) ? checkchip_material[0] : checkchip_material[1];
+
+                }
+            }
+        }
+    }
+
+
+}
