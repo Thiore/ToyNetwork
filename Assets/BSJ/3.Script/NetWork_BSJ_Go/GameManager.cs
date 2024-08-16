1ff7da0522ddbb5d1654a63b7b6bdee2416f9580
백승주 <tmdwn3431@naver.com>
2024-08-16 17:49:08 +0900

    머지 전

--- Assets/BSJ/3.Script/NetWork_BSJ_Go/GameManager.cs
+++ Assets/BSJ/3.Script/NetWork_BSJ_Go/GameManager.cs
@@ -1,33 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Mirror; 
+using Mirror;
 
 public class GameManager : NetworkBehaviour
 {
-    public GameObject coin_Spawn;
+    public GameObject coinPrefab;
 
     [Command]
-    public void CoinSpawn()
+    public void CmdSpawnCoin(Vector3 position)
     {
-        //서버에서 오브젝트 생성
-        GameObject Coin_obj_BSJ = Instantiate(coin_Spawn, Vector3.zero, Quaternion.identity);
+        // 서버에서 오브젝트 생성
+        GameObject coin = Instantiate(coinPrefab, position, Quaternion.identity);
 
-        //네트워크에 오브젝트 추가
-        NetworkServer.Spawn(Coin_obj_BSJ);
+        // 네트워크에 오브젝트 추가
+        NetworkServer.Spawn(coin);
 
-        //생성된 오브젝트의 Identity 가져오기
-        NetworkIdentity identity = Coin_obj_BSJ.GetComponent<NetworkIdentity>();
+        // 생성된 오브젝트의 NetworkIdentity 가져오기
+        NetworkIdentity identity = coin.GetComponent<NetworkIdentity>();
 
-        //클라에게 권한 부여
+        // 클라이언트에게 권한 부여
+        if (identity != null)
+        {
+            RpcAssignClientAuthority(identity.netId);
+        }
+    }
+
+    // 클라이언트에게 권한 부여 요청
+    [ClientRpc]
+    private void RpcAssignClientAuthority(uint netId)
+    {
+        NetworkIdentity identity = NetworkServer.spawned[netId];
         if (identity != null)
         {
             identity.AssignClientAuthority(connectionToClient);
         }
     }
 
-    //클라 권한 해제 메서드 (혹시 모르니까 미리 만들어두기)
-    public void RemoveClientAuthority(NetworkIdentity identity)
+    // 클라이언트 권한 해제 메서드 (혹시 필요하다면 사용)
+    public void ReleaseClientAuthority(NetworkIdentity identity)
     {
         if (identity != null)
         {
--- Assets/BSJ/3.Script/NetWork_BSJ_Go/PlayerManager_BSJ.cs
+++ Assets/BSJ/3.Script/NetWork_BSJ_Go/PlayerManager_BSJ.cs
@@ -2,45 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
-// 서버에서 클라에게 권한을 주기위한
 
 public class PlayerManager_BSJ : NetworkBehaviour
 {
     public override void OnStartLocalPlayer()
     {
         base.OnStartLocalPlayer();
-
-        //클라에게 권한 부여
+        // 클라이언트에서 서버로 권한 부여 요청
         if (isLocalPlayer)
         {
-            //NetworkIdentity identity = GetComponent<NetworkIdentity>();
-            //if (identity != null)
-            //{
-            //    identity.AssignClientAuthority(connectionToClient);
-            //}
-            CmdAssignClientAuthority();
+            CmdRequestClientAuthority();
         }
     }
+
+    // 서버에 클라이언트 권한 부여 요청
     [Command]
-    private void CmdAssignClientAuthority()
+    private void CmdRequestClientAuthority()
     {
-        if (connectionToClient != null)
+        NetworkIdentity identity = GetComponent<NetworkIdentity>();
+        if (identity != null)
         {
-            GetComponent<NetworkIdentity>().AssignClientAuthority(connectionToClient);
+            identity.AssignClientAuthority(connectionToClient);
         }
     }
+
     public override void OnStopLocalPlayer()
     {
         base.OnStopLocalPlayer();
-
-        //권한 해제
+        // 서버에 클라이언트 권한 해제 요청
         if (isLocalPlayer)
         {
-            NetworkIdentity identity = GetComponent<NetworkIdentity>();
-            if (identity != null)
-            {
-                identity.RemoveClientAuthority();
-            }
+            CmdReleaseClientAuthority();
+        }
+    }
+
+    // 서버에 클라이언트 권한 해제 요청
+    [Command]
+    private void CmdReleaseClientAuthority()
+    {
+        NetworkIdentity identity = GetComponent<NetworkIdentity>();
+        if (identity != null)
+        {
+            identity.RemoveClientAuthority();
         }
     }
 }
