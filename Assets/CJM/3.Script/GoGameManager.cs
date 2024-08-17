088010c533e631b1849fbf5b69b32627db7c4c9f
최정문 <>
2024-08-17 17:47:37 +0900

    Layer 활용 고려

--- Assets/CJM/3.Script/GoGameManager.cs
+++ Assets/CJM/3.Script/GoGameManager.cs
@@ -3,8 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoGameManager : MonoBehaviour
+public class GoGameManager : NetworkBehaviour
 {
+    public int myTurn = 1;
+
     private float currentTime = 0f;
     private float limitTime = 10f;
 
@@ -14,29 +16,11 @@ public class GoGameManager : MonoBehaviour
 
     private void Awake()
     {
+        Debug.Log(myTurn % 2);
         logic = GameObject.FindObjectOfType<Gomoku_Logic>();
         player = GameObject.FindObjectOfType<Player>();
     }
 
-
-    public void GoGame()
-    {
-        StartCoroutine(GoGame_co());
-    }
-
-    private IEnumerator GoGame_co()
-    {
-        while(currentTime < limitTime)
-        {
-            currentTime += Time.deltaTime;
-            if (currentTime >= limitTime)
-            {
-                Debug.Log(currentTime);
-                currentTime = 0f;
-                yield return new WaitForSecondsRealtime(limitTime);
-            }
-        }
-    }
 
 
 
--- Assets/Mango/3.Script/Player.cs
+++ Assets/Mango/3.Script/Player.cs
@@ -1,10 +1,11 @@
+using Mirror;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+using UnityEngine.UIElements;
 
-public class Player : MonoBehaviour
+public class Player : NetworkBehaviour
 {
     public Color color;
     public enum Color
@@ -18,57 +19,64 @@ public class Player : MonoBehaviour
     //0Èæ 1¹é
     [SerializeField] private Material[] chip_material;
 
+    private LayerMask empty = LayerMask.NameToLayer("Empty");
+
     // false Èæ true ¹é 
     private bool myTurn = true;
     public bool Myturn { get => myTurn; }
 
-    [SerializeField] Gomoku_Logic logic;
+    [SerializeField] private Gomoku_Logic logic;
+    private GoGameManager myGameManager;
 
     private float currentTime = 0f;
     private float limitTime = 10f;
 
+
     private void Awake()
     {
         logic = GameObject.FindObjectOfType<Gomoku_Logic>();
+        myGameManager = GameObject.FindObjectOfType<GoGameManager>();
     }
 
     private void Update()
     {
+        if (!isLocalPlayer) return;
+
         if (Input.GetMouseButtonUp(0) && logic.result_Panel.activeSelf.Equals(false))
         {
-
-            PutChip();
-
-        }
-    }
-
-    private void PutChip()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {
-            if (hit.collider != null)
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, empty))
             {
-                DrawBoard(hit.collider.gameObject);
+                if (hit.collider != null)
+                {
+                    CmdPutChip(hit.collider.gameObject);
+                }
             }
         }
     }
 
-
-    private void DrawBoard(GameObject obj)
+    public int GetColor()
     {
-        if (obj.TryGetComponent(out Chip chip) && !chip.IsPut)
-        {
-            obj.GetComponent<MeshFilter>().mesh = chip.ChipMesh;
-            MeshRenderer mate = chip.GetComponent<MeshRenderer>();
-            mate.material = myTurn ? chip_material[0] : chip_material[1];
-            logic.AddChip(chip, this);
-            chip.IsPut = true;
-            TurnChange();
-        }
+        return myColor;
     }
 
+    [Command]
+    private void CmdPutChip(GameObject obj)
+    {
+        RPCDrawBoard(obj);
+    }
+
+    [ClientRpc]
+    private void RPCDrawBoard(GameObject obj)
+    {
+        MeshRenderer mate = obj.GetComponent<MeshRenderer>();
+        mate.enabled = true;
+        mate.material = myTurn ? chip_material[0] : chip_material[1];
+        //logic.AddChip(chip, this);
+        TurnChange();
+
+    }
 
 
     public void TurnChange()
@@ -87,14 +95,13 @@ public class Player : MonoBehaviour
         {
             if (hit.collider != null)
             {
-                if(hit.collider.gameObject.TryGetComponent<Chip>(out Chip chip))
+                if (hit.collider.gameObject.TryGetComponent<Chip>(out Chip chip))
                 {
                     if (!chip.IsPut && logic.result_Panel.activeSelf.Equals(false))
                     {
-                        transform.GetComponent<MeshFilter>().mesh = chip.ChipMesh;
                         MeshRenderer mate = transform.GetComponent<MeshRenderer>();
-
-                        mate.material = myTurn ? chip.CheckChip_Mate[0] : chip.CheckChip_Mate[1];
+                        mate.enabled = true;
+                        mate.material = ((myGameManager.myTurn % 2).Equals(1)) ? chip.CheckChip_Mate[0] : chip.CheckChip_Mate[1];
                     }
                 }
             }
