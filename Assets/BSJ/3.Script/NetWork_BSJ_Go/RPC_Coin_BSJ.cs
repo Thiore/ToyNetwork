90879ad60feeda1a669918bdbf411c1e57e4d8f0
백승주 <tmdwn3431@naver.com>
2024-08-17 20:28:05 +0900

    [update] 선/공 coin 생성

--- Assets/BSJ/3.Script/NetWork_BSJ_Go/RPC_Coin_BSJ.cs
+++ Assets/BSJ/3.Script/NetWork_BSJ_Go/RPC_Coin_BSJ.cs
@@ -6,13 +6,26 @@ using Mirror;
 
 public class RPC_Coin_BSJ : NetworkBehaviour
 {
-    [SerializeField] private GameObject Coin_BSJ;
-    //Client가 Server에 Connect 되었을 때 콜백함수
+    [SerializeField] private GameObject coinPrefab; // 코인 프리팹 변수명 수정
+    
+    // Client가 Server에 Connect 되었을 때 콜백 함수
     public override void OnStartAuthority()
     {
         if (isLocalPlayer)
         {
-            Coin_BSJ.SetActive(true);
+            Debug.Log("OnStartAuthority 들어옴");
+            CmdSpawnCoin();
         }
     }
+
+    [Command]
+    private void CmdSpawnCoin()
+    {
+        GameObject coin = Instantiate(coinPrefab);
+        NetworkServer.Spawn(coin);
+    }
+
+    
+
+
 }
