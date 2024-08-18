d66ecb54cb46b322debf2f7011ef2a8199040065
백승주 <tmdwn3431@naver.com>
2024-08-18 21:54:06 +0900

    [update] 머테리얼 바꾸기 / 클라1에서 둘 다 White / 클라2에서는 정상

--- Assets/BSJ/3.Script/Select_color.cs
+++ Assets/BSJ/3.Script/Select_color.cs
@@ -1,23 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-    public enum PlayerType { Black, White}
+using Mirror;
 
-public class Select_color : MonoBehaviour
+public enum PlayerType { Black, White }
+
+public class Select_color : NetworkBehaviour
 {
+
     public PlayerType playerType;
 
     public Material chipBlack;
     public Material chipWhite;
     public GameObject chipPrefab;
     private Renderer chipRenderer;
-    
 
-   
     private void Start()
     {
+        Debug.Log($"Start - PlayerType: {playerType}, isLocalPlayer: {hasAuthority}");
+        // 오브젝트가 권한을 가지고 있는지 확인
+        if (hasAuthority)
+        {
+            // 로컬 플레이어의 정보나 PlayerType을 설정
+            CmdSetPlayerType(PlayerType.White); // 예시로 White로 설정
+        }
+
         chipRenderer = chipPrefab.GetComponent<Renderer>();
         SetPlayerLayer();
+        SetChipMaterial();
+    }
+
+    [Command]
+    private void CmdSetPlayerType(PlayerType type)
+    {
+        Debug.Log("CmdSetPlayerType 호출됨. 타입: " + type);
+        playerType = type;
+        RpcUpdatePlayerType(type);
+    }
+
+    [ClientRpc]
+    void RpcUpdatePlayerType(PlayerType newType)
+    {
+        playerType = newType;
+        Debug.Log($"RpcUpdatePlayerType 호출됨. 타입: {playerType}");
+
+        // PlayerType이 업데이트된 후 즉시 material을 업데이트
         SetChipMaterial();
     }
 
@@ -35,6 +62,7 @@ public class Select_color : MonoBehaviour
 
     private void SetChipMaterial()
     {
+        Debug.Log($"SetChipMaterial 호출됨 - PlayerType: {playerType}");
         if (playerType == PlayerType.Black)
         {
             chipRenderer.material = chipBlack;
