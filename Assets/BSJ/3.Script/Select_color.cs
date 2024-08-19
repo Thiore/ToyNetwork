933cbe8628cd84e3ad4f38b2162a8d5afa61c15b
백승주 <tmdwn3431@naver.com>
2024-08-19 12:09:43 +0900

    클라1에서 정상 / 클라2 모두 black으로 나옴

--- Assets/BSJ/3.Script/Select_color.cs
+++ Assets/BSJ/3.Script/Select_color.cs
@@ -13,49 +13,106 @@ public class Select_color : NetworkBehaviour
     public Material chipBlack;
     public Material chipWhite;
     public GameObject chipPrefab;
-    private Renderer chipRenderer;
-    public override void OnStartAuthority()
-    {
-        if (hasAuthority)
-        {
-            Debug.Log("Select_color OnStartAuthority 들어옴");
-            //CmdSpawnCoin();
-        }
-    }
+    public Renderer chipRenderer;
+    
+    private static int playerCount = 0;
+    
+
     private void Start()
     {
-        Debug.Log($"Start - PlayerType: {playerType}, isLocalPlayer: {hasAuthority}");
-        // 오브젝트가 권한을 가지고 있는지 확인
-        if (hasAuthority)
-        {
-            // 로컬 플레이어의 정보나 PlayerType을 설정
-            CmdSetPlayerType(PlayerType.White); // 예시로 White로 설정
-        }
+        //if (chipPrefab == null)
+        //{
+        //    Debug.LogError("chipPrefab이 할당되지 않았습니다!");
+        //    return;
+        //}
         
-
+        //if (chipRenderer == null)
+        //{
+        //    Debug.LogError("chipPrefab에서 Renderer 컴포넌트를 찾을 수 없습니다!");
+        //    return;
+        //}
         chipRenderer = chipPrefab.GetComponent<Renderer>();
-        SetPlayerLayer();
-        SetChipMaterial();
+        if (isServer)
+        {
+            AssignPlayerType();
+
+        }
+        //else
+        //{
+        //    OnPlayerTypeChanged(playerType);
+        //}
+
+        //Debug.Log($"Start - PlayerType: {playerType}, isLocalPlayer: {hasAuthority}");
+        // 오브젝트가 권한을 가지고 있는지 확인
+        //if (hasAuthority)
+        //{
+        //    // 로컬 플레이어의 정보나 PlayerType을 설정
+        //    Debug.Log("CmdSetPlayerType 호출 전"); //호출 전 상태 확인
+        //    CmdSetPlayerType(PlayerType.White); // 예시로 White로 설정
+        //    Debug.Log("CmdSetPlayerType 호출 후");
+        //    Debug.Log($"Start - PlayerType: {playerType}, isLocalPlayer: {hasAuthority}");
+        //}
+
+
+        //SetPlayerLayer();
+        //SetChipMaterial();
+
+
     }
+
+    [Server]
+    private void AssignPlayerType()
+    {
+        //첫 번째 플레이어는 흰색, 두 번째 플레이어는 검은색
+        if (playerCount % 2 == 0)
+        {
+            playerType = PlayerType.White;
+        }
+        else
+        {
+            playerType = PlayerType.Black;
+        }
+        playerCount++;
+        //모든 클라에게 업데이트된 플레이어 타입 보내주기
+        RpcUpdatePlayerType(playerType);
+    }
+
 
     [Command]
     private void CmdSetPlayerType(PlayerType type)
     {
+        Debug.Log($"권한 확인 - hasAuthority: {hasAuthority}");
         Debug.Log("CmdSetPlayerType 호출됨. 타입: " + type);
         playerType = type;
         RpcUpdatePlayerType(type);
     }
+    
 
     [ClientRpc]
-    void RpcUpdatePlayerType(PlayerType newType)
+   public void RpcUpdatePlayerType(PlayerType newType)
     {
-        playerType = newType;
-        Debug.Log($"RpcUpdatePlayerType 호출됨. 타입: {playerType}");
+        //// 서버에서 결정된 플레이어 타입을 클라이언트에 반영
+        //playerType = newType;
+        //Debug.Log($"RpcUpdatePlayerType 호출됨. 타입: {playerType}");
 
-        // PlayerType이 업데이트된 후 즉시 material을 업데이트
-        SetChipMaterial();
+        //// PlayerType이 업데이트된 후 material을 업데이트
+        //SetPlayerLayer();
+        //SetChipMaterial();
+
+        // 서버에서 결정된 플레이어 타입을 클라이언트에 반영합니다.
+        OnPlayerTypeChanged(newType);
+
     }
 
+    public void OnPlayerTypeChanged(PlayerType newType)
+    {
+        playerType = newType;
+        Debug.Log($"OnPlayerTypeChanged 호출됨. 타입: {playerType}");
+
+        // PlayerType이 업데이트된 후 material을 업데이트
+        SetPlayerLayer();
+        SetChipMaterial();
+    }
     private void SetPlayerLayer()
     {
         if (playerType == PlayerType.Black)
@@ -68,7 +125,7 @@ public class Select_color : NetworkBehaviour
         }
     }
 
-    private void SetChipMaterial()
+    private  void SetChipMaterial()
     {
         Debug.Log($"SetChipMaterial 호출됨 - PlayerType: {playerType}");
         if (playerType == PlayerType.Black)
